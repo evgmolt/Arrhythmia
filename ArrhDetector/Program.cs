245c3cf55318ce6c523e94fe8e24c27e32389267
evgmolt <evg.molt@gmail.com>
2022-10-18 09:18:05 +0400

    Reading file, detect intervals

---
+++ ArrhDetector/Program.cs
@@ -0,0 +1,19 @@
+﻿using ArrhDetector;
+
+int SamplingFreq = 360;
+string FileName = "100.csv";
+string[] Lines = File.ReadAllLines(FileName);
+double[] Data = new double[Lines.Length];
+for (int i = 1; i < Lines.Length; i++)
+{
+    string line = Lines[i];
+    string[] parts = line.Split(',');
+    Data[i] = int.Parse(parts[1]) - 1024;
+}
+Console.WriteLine("Всего отсчетов : " + Lines.Length);
+WaveDetector WD = new(SamplingFreq);
+for (int i = 0; i < Data.Length; i++)
+{
+    WD.Detect(Data, i);
+}
+Console.WriteLine("Интервалов обнаружено " + WD.FiltredPoints.Count.ToString());
---
+++ ArrhDetector/WaveDetectorEventArgs.cs
@@ -0,0 +1,10 @@
+﻿namespace ArrhDetector
+{
+    internal class WaveDetectorEventArgs : EventArgs
+    {
+        public int WaveCount { get; set; }
+        public int Interval { get; set; }
+        public double Amplitude { get; set; }
+        public int Arrithmia { get; set; }
+    }
+}
